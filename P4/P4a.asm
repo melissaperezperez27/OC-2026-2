7c0471cada78f4963c017a1408bd642cc51ae1c2
melissaperezperez27 <melissa.perez.perez@uabc.edu.mx>
2026-09-11 01:09:30 +0000

    modified:   P4/P4 	modified:   P4/P4a.asm 	new file:   P4/P4a.o

--- P4/P4a.asm
+++ P4/P4a.asm
@@ -11,7 +11,7 @@ _start:
 				
 	mov byte [msg], 'Z'	; cambia la primera letra 'a' por 'Z'
 	mov edx, msg		; edx = dirección de la cadena msg
-	call msg			; imprime cadena modificada msg terminada en valor nulo (0)
+	call puts			; imprime cadena modificada msg terminada en valor nulo (0)
 
 	mov	eax, 1	    	; seleccionar llamada al sistema para fin de programa
 	int	0x80        	; llamada al sistema - fin de programa
