936b018632f5d11a9ca5eb8ee73cf2baf63c59bd
melissaperezperez27 <melissa.perez.perez@uabc.edu.mx>
2026-09-11 01:47:17 +0000

    modified:   P4/P4c.asm 	modified:   P4/P4d.asm

--- P4/P4d.asm
+++ P4/P4d.asm
@@ -10,12 +10,10 @@ _start:
     
     mov ebx, msg        ; ebx = registro base (inicio de la cadena)
     mov ecx, 25         ; ecx = registro índice (posición de la letra 'z')
-    mov byte [ebx+ecx], 'Z' ; 
+    mov byte [ebx+ecx], 'Z' ; cambia 'z' por 'Z' utilizando direccionamiento base mas índice 
+    mov edx, msg        ; edx = dirección de la cadena msg
+    call puts           ; imprime la cadena modificada msg terminada en valor nulo (0)
     
-    mov edx, msg
-    call puts
-    
-
 	mov	eax, 1	    	; seleccionar llamada al sistema para fin de programa
 	int	0x80        	; llamada al sistema - fin de programa
 
