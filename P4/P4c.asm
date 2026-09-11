ffee2ddbad9d5757d6259129dce248aadbb9c8f5
melissaperezperez27 <melissa.perez.perez@uabc.edu.mx>
2026-09-11 01:36:19 +0000

    modified:   P4/P4c.asm

--- P4/P4c.asm
+++ P4/P4c.asm
@@ -7,8 +7,11 @@ section	.text
 _start:                   
 	mov edx, msg		; edx = dirección de la cadena msg
 	call puts			; imprime la cadena original msg terminada en valor nulo (0)
-    
-   
+
+    mov eax, msg        ; eax = dirección de la cadena msg
+    mov byte [eax+26], '@' ; cambia '0' por '@' utilizandodireccionamiento	relativo a registro
+    mov edx, msg        ; edx = dirección de la cadena msg
+    call puts           ; imprime la cadena modificada msg terminada en valor nulo (0)
 
 	mov	eax, 1	    	; seleccionar llamada al sistema para fin de programa
 	int	0x80        	; llamada al sistema - fin de programa
