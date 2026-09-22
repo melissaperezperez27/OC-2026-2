bfec79c223fb1bcb7832be6b1e64d558d8701d84
melissaperezperez27 <melissa.perez.perez@uabc.edu.mx>
2026-09-22 21:12:44 +0000

    modified:   P5/P5.asm

--- P5/P5.asm
+++ P5/P5.asm
@@ -6,11 +6,30 @@ section	.text
 
 _start:                     ;tell linker entry point
 
-	mov ebx, 0x5C4B2A60 
-    add ebx, 0x2228661
-    push ebx              
-    mov byte edx  
+
+	mov ebx, 0x5C4B2A60     ; inciso a
+    add ebx, 0x02228661
+    mov eax, ebx
+    call pHex_dw
+
+    push bx                 ; inciso b
+                            . inciso c
+    inc N
+    call pHex_dw
+
+    mov al, cl               ; inciso c
+    mov cl, 8
+    mult 
+
+
+
+    mov edx, 0xFF
+    div bx
+
+
 
 	mov eax, 1	;system call number (sys_exit) -- fin del programa
 	int 0x80        ;call kernel
 
+section	.data
+N	db 0
