027dc60dc25af2e3a6df0131db3c435c876c207d
melissaperezperez27 <melissa.perez.perez@uabc.edu.mx>
2026-09-22 21:28:27 +0000

    deleted:    P5/P5 	modified:   P5/P5.asm 	deleted:    P5/P5.o

--- P5/P5.asm
+++ P5/P5.asm
@@ -11,21 +11,28 @@ _start:                     ;tell linker entry point
     add ebx, 0x02228661
     mov eax, ebx
     call pHex_dw
-    
+    mov eax, 1
 
-    ; push bx                 ; inciso b
-                            ; inciso c
-    ; inc N
-    ; call pHex_dw
+    push bx                 ; inciso b
+    mov eax, 1
+                            
+    inc N
+    call pHex_dw
+    mov eax, 1
 
-    ; mov al, cl               ; inciso c
-    ; mov cl, 8
-    ; mult 
+    mov al, cl               ; inciso c
+    mov cl, 8
+    mul cl
+    mov [N], cl 
+    call pHex_dw
+    mov eax, 1
 
 
 
     ; mov edx, 0xFF
     ; div bx
+
+    ; pop bx 
 
 	mov eax, 1	;system call number (sys_exit) -- fin del programa
 	int 0x80        ;call kernel
