486683abc164b1135cc557c4344ab007952799d1
melissaperezperez27 <melissa.perez.perez@uabc.edu.mx>
2026-09-22 20:56:54 +0000

    modified:   P5/P5.asm 	modified:   P4/P4.asm 	modified:   P5/P5.asm

--- P5/P5.asm
+++ P5/P5.asm
@@ -8,6 +8,8 @@ _start:                     ;tell linker entry point
 
 	mov ebx, 0x5C4B2A60 
     add ebx, 0x2228661
+    push ebx              ; a
+    mov byte edx  
 
 	mov eax, 1	;system call number (sys_exit) -- fin del programa
 	int 0x80        ;call kernel
