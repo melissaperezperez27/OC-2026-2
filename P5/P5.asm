fcf2bb29a41750ab5dfd8f30b1065e5fe3c9f4de
melissaperezperez27 <melissa.perez.perez@uabc.edu.mx>
2026-09-23 02:53:12 +0000

    new file:   P5/P5 	modified:   P5/P5.asm 	modified:   P5/P5.o

--- P5/P5.asm
+++ P5/P5.asm
@@ -1,45 +1,20 @@
-%include "../LIB/pc_iox.inc"
+%include "../LIB/pc_iox.inc"    ; incluir declaraciones de procedimiento externos
 
 section	.text
 
-	global _start       ;must be declared for using gcc
+	global _start               ; referencia para inicio de programa
 
-_start:                     ;tell linker entry point
+_start:                     
+    ; Inciso a
+	mov ebx, 0x5C4B2A60         ; ebx = 0x5C4B2A60                                                      
+    add ebx, 0x02228661         ; ebx = ebx + 0x02228661
+    mov eax, ebx                ; eax = ebx
+    call pHex_dw                ; mostrar en pantalla
+    mov eax, 0x0a               ; 
+    call putchar
 
-
-	mov ebx, 0x5C4B2A60     ; inciso a
-    add ebx, 0x02228661
-    mov eax, ebx
-    call pHex_dw
-    
-
-    push bx                 ; inciso b
- 
-    call pHex_dw
-                            
-    inc word [N] 
-    call pHex_dw
-    mov eax, 1
-
-    mov al, cl               ; inciso c
-    mov cl, 8
-    mul cl
-    mov [N], cl 
-
-    movzx eax, word [N]
-    
-    inc word [N]
-    
-
-    ; mov edx, 0xFF
-    ; div bx
-
-    ; pop bx 
-
-
+    mov ebx, 0
 	mov eax, 1
 	int 0x80       
 
-section	.data
-N db 0
-salto db 0
+
